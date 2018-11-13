1063e8649ca0c46eb7812973ebed3c6ba42e3c38
Volodymyr Dedenko <dedenkof@ukr.net>
2018-11-13 17:15:04 +0200

    Change all notice to gender

--- js/main.js
+++ js/main.js
@@ -19,542 +19,542 @@ $(function () {
     var people = [{
 
         "fname": "Иноземцева Надежда",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Ярцева Аня",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Наталья Мальцова",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Марина Губачева",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Стаина Анна",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Шеркова Евгения",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Андрюхина Нина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Головина Анна",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Чупрова Екатерина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Холопова Виктория",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Крупина Мария",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Полевщикова Кристина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Пьянкова Диана",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Буланова Яна",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Щеголева Светлана",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Янкелевич Алина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Якушевич Наталья",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Фомичева Диана",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Пережогина Виктория",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Рябова Дарья",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Домышева Юлия",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Милова Татьяна",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Шипицына Анна",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Протасова Евгения",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Коржева Ксения",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Кузнецова Вероника",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Сурина Алиса",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Перова Агата",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Коржакова Ольга",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Абрамович Валентина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Крылова Наталья",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Проскуркина Александра",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Терехова Юлия",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Труфанова Варвара",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Батурина Марина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Васнецова Нина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Перевалова Надежда",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Рошета Любовь",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Мосякова Татьяна",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Носова Анастасия",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Типалова Юнона",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Колесникова Инесса",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Якуничева Анна",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Левина Евгения",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Дурмина Антонина",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Игошина Мария",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Званцова Светлана",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Хлопонина Елена",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Суботина Изабелла",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Дроздова Марфа",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Милютина Изабелла",
-        "sex": 0
+        "gender": 0
 
     }, {
 
         "fname": "Домаш Вячеслав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Лагутов Руслан",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Степанков Радислав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Перешивкин Ростислав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Кобзев Платон",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Кабанов Игнатий",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Чепурин Николай",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Крымов Изяслав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Собчак Евгений",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Былинкин Максим",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Архипов Сергей",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Донцов Самсон",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Лызлов Владислав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Фризов Владимир",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Крупнов Дмитрий",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Ябловский Вадим",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Гусенков Самсон",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Панфёров Семён",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Ясинский Павел",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Тетерев Глеб",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Шлыков Николай",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Козлов Илья",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Паулкин Ефим",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Саламатов Николай",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Сподарев Степан",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Бочкарёв Владимир",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Опекунов Вячеслав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Телицын Тимофей",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Ямлиханов Андрей",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Малиновский Владислав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Гребнев Ростислав",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Кузанов Леонид",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Цитников Всеволод",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Банин Александр",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Капица Кирилл",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Володин Зиновий",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Уланов Иван",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Седых Кирилл",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Туровский Лев",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Москвин Артем",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Цыганов Егор",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Голумбовский Дмитрий",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Аничков Кирилл",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Канаш Степан",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Клепахов Дмитрий",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Колесников Анатолий",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Цветков Леонид",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Мишин Ефим",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Кирилл Ловец",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Евгений Кукарин",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Сергей Прохоров",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Андрей Князев",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Михаил Павленко",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Виктор Бодров",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Алексей Комаров",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Дмитрий Маслов",
-        "sex": 1
+        "gender": 1
 
     }, {
 
         "fname": "Борисов Евгений",
-        "sex": 1
+        "gender": 1
 
     }];
 
@@ -607,7 +607,7 @@ $(function () {
 
 
         // output HTML
-        this.generateHTML = function (fname, avatar, sex, city, phrasex, randOrderSum) {
+        this.generateHTML = function (fname, avatar, gender, city, phragender, randOrderSum) {
 
 
             // Function rand opacity background depending on people
@@ -618,15 +618,15 @@ $(function () {
 
 
 
-            if (sex == 0) {
+            if (gender == 0) {
 
-                var classSex = 'woman-item';
+                var classgender = 'woman-item';
 
                 var bgrand = 'style="background: rgba(255,222,38,' + randomOpacity(0.4, 1) + ');"';
 
             } else {
 
-                var classSex = 'man-item';
+                var classgender = 'man-item';
 
                 var bgrand = 'style="background: rgba(57,199,255,' + randomOpacity(0.4, 1) + ');"';
 
@@ -636,14 +636,14 @@ $(function () {
 
 
 
-                '<div '  + bgrand + ' class="popnotice-item ' + classSex + '">' +
+                '<div '  + bgrand + ' class="popnotice-item ' + classgender + '">' +
 
                 '<img src="' + avatar + '" class="popnotice-img">' +
 
                 '<div class="popnotice-description">' +
 
 
-                '<div class="popnotice-text"><h3>' + fname +'</h3>'+ 'г. ' + city + ', '  + phrasex + '</div>' +
+                '<div class="popnotice-text"><h3>' + fname +'</h3>'+ 'г. ' + city + ', '  + phragender + '</div>' +
 
 
                 '</div>' +
@@ -716,8 +716,8 @@ $(function () {
 
         }
 
-        // img-icon path depending on the sex
-        var avatar = item.sex ? imgMan : imgWoman;
+        // img-icon path depending on the gender
+        var avatar = item.gender ? imgMan : imgWoman;
 
         var city = cityList[self.showItem];
 
@@ -846,16 +846,16 @@ $(function () {
 
 
 
-        // function output verb depending on the sex people and price goods depending on the action noun
+        // function output verb depending on the gender people and price goods depending on the action noun
 
         this.nounSum = function (aNoun){
             if(aNoun == 'покупку' || aNoun == 'заказ' || aNoun == 'предзаказ товара' || aNoun == 'товар'){
 
-                return phrasex  = item.sex ? aVerb + ' ' + aNoun + ' на сумму ' + sum + ' грн.' : aVerb + 'а ' + aNoun + ' на сумму ' + sum + ' грн.';
+                return phragender  = item.gender ? aVerb + ' ' + aNoun + ' на сумму ' + sum + ' грн.' : aVerb + 'а ' + aNoun + ' на сумму ' + sum + ' грн.';
 
             }else{
 
-                return phrasex  = item.sex ? aVerb + ' ' + aNoun : aVerb + 'а ' + aNoun ;
+                return phragender  = item.gender ? aVerb + ' ' + aNoun : aVerb + 'а ' + aNoun ;
 
             }
         };
@@ -869,7 +869,7 @@ $(function () {
         self.showItem++;
 
         // Output generation HTML code with passing an argument popnotice item to a function
-        var html = self.generateHTML(item.fname, avatar, item.sex, city, phrasex);
+        var html = self.generateHTML(item.fname, avatar, item.gender, city, phragender);
 
         // Call appear and disappear function
         self.addItem(html);
